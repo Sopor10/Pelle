d4d190c56f5edc0d33c39d9a1ec0d100aa291b5b
Lars Lehmann <lars.lehmann@smapone.com>
2023-02-10 13:09:52 +0100

    first version

---
+++ Application/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Application;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+    }
+
+    private void WebView_OnNavigating(object sender, WebNavigatingEventArgs e)
+    {
+        if (e.Url.Contains("vid"))
+        {
+            e.Cancel = true;
+            
+            Navigation.PushAsync(new VideoPage()
+            {
+                BindingContext = new VideoViewModel(e.Url)
+            });
+            
+        }
+    }
+}
---
+++ Application/VideoPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Application;
+
+using System.Net;
+
+public partial class VideoPage : ContentPage
+{
+
+    public VideoPage()
+    {
+        InitializeComponent();
+        
+    }
+    
+}
+
+public record VideoViewModel(string FileName)
+{
+    
+    public string FilePath
+    {
+        get
+        {
+            var currentAppDataDirectory = Microsoft.Maui.Storage.FileSystem.Current.AppDataDirectory;
+            var filename = FileName.Split("/").Last();
+            
+            return WebUtility.UrlDecode(Path.Join(currentAppDataDirectory, "vid", filename));
+        }
+    }
+}
