22ea69e0d2b30b4e472430515ddbb3ba9d9d33cf
Daniel Gilbert <daniel.gilbert@smapone.com>
2024-04-04 14:14:55 +0200

    Make the App run again!

--- Application/MauiProgram.cs
+++ Application/MauiProgram.cs
@@ -18,6 +18,8 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        builder.Services.AddMauiBlazorWebView();
+
         return builder.Build();
     }
 }
